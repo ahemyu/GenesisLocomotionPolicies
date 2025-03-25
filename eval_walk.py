815dd39a92203629abb71bb55334c67af9557d56
ahemyu <hansovic7@gmail.com>
2025-03-25 18:19:51 +0000

    trained walking on flat ground with simple go2env

---
+++ eval_walk.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+import pickle
+import torch
+from rsl_rl.runners import OnPolicyRunner
+import genesis as gs
+from simple_go2_env import Go2Env
+
+### GPU VERSION ###
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
+    parser.add_argument("-r", "--record", action="store_true", default=True)
+    parser.add_argument("--ckpt", type=int, default=100)
+    args = parser.parse_args()
+
+    gs.init()
+
+    log_dir = f"logs/{args.exp_name}"
+    env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg = pickle.load(open(f"logs/{args.exp_name}/cfgs.pkl", "rb"))
+    reward_cfg["reward_scales"] = {}
+
+    env = Go2Env(
+        num_envs=1,
+        env_cfg=env_cfg,
+        obs_cfg=obs_cfg,
+        reward_cfg=reward_cfg,
+        command_cfg=command_cfg,
+        show_viewer=False,
+    )
+
+    args.max_iterations = 1
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device="cuda:0")
+    resume_path = os.path.join(log_dir, f"model_{args.ckpt}.pt")
+    runner.load(resume_path)
+    policy = runner.get_inference_policy(device="cuda:0")
+
+    env.reset()
+    obs = env.get_observations()
+    with torch.no_grad():
+        stop = False
+        n_frames = 0
+        if args.record:
+            env.start_recording(record_internal=False)
+        while not stop:
+            actions = policy(obs)
+            obs, _, rews, dones, infos = env.step(actions)
+            n_frames += 1
+            if args.record:
+                if n_frames == 200:
+                    env.stop_recording("go2_walking.mp4")
+                    exit()
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+python eval_walk.py -e go2-walking -r --ckpt 100
+"""
--- train_walk.py
+++ train_walk.py
@@ -1,61 +1,56 @@
 import argparse
-import copy
 import os
 import pickle
 import shutil
 
-import numpy as np
-import torch
-import wandb
-from reward_wrapper import Go2
-from locomotion_env import LocoEnv
+from simple_go2_env import Go2Env
 from rsl_rl.runners import OnPolicyRunner
 
 import genesis as gs
 
 
-def get_train_cfg(args):
+def get_train_cfg(exp_name, max_iterations):
 
     train_cfg_dict = {
-        'algorithm': {
-            'clip_param': 0.2,
-            'desired_kl': 0.01,
-            'entropy_coef': 0.01,
-            'gamma': 0.99,
-            'lam': 0.95,
-            'learning_rate': 0.001,
-            'max_grad_norm': 1.0,
-            'num_learning_epochs': 5,
-            'num_mini_batches': 4,
-            'schedule': 'adaptive',
-            'use_clipped_value_loss': True,
-            'value_loss_coef': 1.0,
+        "algorithm": {
+            "clip_param": 0.2,
+            "desired_kl": 0.01,
+            "entropy_coef": 0.01,
+            "gamma": 0.99,
+            "lam": 0.95,
+            "learning_rate": 0.001,
+            "max_grad_norm": 1.0,
+            "num_learning_epochs": 5,
+            "num_mini_batches": 4,
+            "schedule": "adaptive",
+            "use_clipped_value_loss": True,
+            "value_loss_coef": 1.0,
         },
-        'init_member_classes': {},
-        'policy': {
-            'activation': 'elu',
-            'actor_hidden_dims': [512, 256, 128], # kleines model oft besser
-            'critic_hidden_dims': [512, 256, 128],
-            'init_noise_std': 1.0,
+        "init_member_classes": {},
+        "policy": {
+            "activation": "elu",
+            "actor_hidden_dims": [512, 256, 128],
+            "critic_hidden_dims": [512, 256, 128],
+            "init_noise_std": 1.0,
         },
-        'runner': {
-            'algorithm_class_name': 'PPO',
-            'checkpoint': -1,
-            'experiment_name': args.exp_name,
-            'load_run': -1,
-            'log_interval': 1,
-            'max_iterations': args.max_iterations,
-            'num_steps_per_env': 24,
-            'policy_class_name': 'ActorCritic',
-            'record_interval': 50,
-            'resume': False,
-            'resume_path': None,
-            'run_name': '',
-            'runner_class_name': 'runner_class_name',
-            'save_interval': 100,
+        "runner": {
+            "algorithm_class_name": "PPO",
+            "checkpoint": -1,
+            "experiment_name": exp_name,
+            "load_run": -1,
+            "log_interval": 1,
+            "max_iterations": max_iterations,
+            "num_steps_per_env": 24,
+            "policy_class_name": "ActorCritic",
+            "record_interval": -1,
+            "resume": False,
+            "resume_path": None,
+            "run_name": "",
+            "runner_class_name": "runner_class_name",
+            "save_interval": 100,
         },
-        'runner_class_name': 'OnPolicyRunner',
-        'seed': 1,
+        "runner_class_name": "OnPolicyRunner",
+        "seed": 1,
     }
 
     return train_cfg_dict
@@ -63,210 +58,78 @@ def get_train_cfg(args):
 
 def get_cfgs():
     env_cfg = {
-        'urdf_path': 'urdf/go2/urdf/go2.urdf',
-        'links_to_keep': ['FL_foot', 'FR_foot', 'RL_foot', 'RR_foot',],
-        'num_actions': 12,
-        'num_dofs': 12,
+        "num_actions": 12,
         # joint/link names
-        'default_joint_angles': {  # [rad]
-            'FL_hip_joint': 0.0,
-            'FR_hip_joint': 0.0,
-            'RL_hip_joint': 0.0,
-            'RR_hip_joint': 0.0,
-            'FL_thigh_joint': 0.8,
-            'FR_thigh_joint': 0.8,
-            'RL_thigh_joint': 1.0,
-            'RR_thigh_joint': 1.0,
-            'FL_calf_joint': -1.5,
-            'FR_calf_joint': -1.5,
-            'RL_calf_joint': -1.5,
-            'RR_calf_joint': -1.5,
+        "default_joint_angles": {  # [rad]
+            "FL_hip_joint": 0.0,
+            "FR_hip_joint": 0.0,
+            "RL_hip_joint": 0.0,
+            "RR_hip_joint": 0.0,
+            "FL_thigh_joint": 0.8,
+            "FR_thigh_joint": 0.8,
+            "RL_thigh_joint": 1.0,
+            "RR_thigh_joint": 1.0,
+            "FL_calf_joint": -1.5,
+            "FR_calf_joint": -1.5,
+            "RL_calf_joint": -1.5,
+            "RR_calf_joint": -1.5,
         },
-        'dof_names': [
-            'FR_hip_joint',
-            'FR_thigh_joint',
-            'FR_calf_joint',
-            'FL_hip_joint',
-            'FL_thigh_joint',
-            'FL_calf_joint',
-            'RR_hip_joint',
-            'RR_thigh_joint',
-            'RR_calf_joint',
-            'RL_hip_joint',
-            'RL_thigh_joint',
-            'RL_calf_joint',
+        "dof_names": [
+            "FR_hip_joint",
+            "FR_thigh_joint",
+            "FR_calf_joint",
+            "FL_hip_joint",
+            "FL_thigh_joint",
+            "FL_calf_joint",
+            "RR_hip_joint",
+            "RR_thigh_joint",
+            "RR_calf_joint",
+            "RL_hip_joint",
+            "RL_thigh_joint",
+            "RL_calf_joint",
         ],
-        'termination_contact_link_names': ['base'],
-        'penalized_contact_link_names': ['base', 'thigh', 'calf'],
-        'feet_link_names': ['foot'],
-        'base_link_name': ['base'],
         # PD
-        'PD_stiffness': {'joint': 30.0},
-        'PD_damping': {'joint': 1.5},
-        'use_implicit_controller': False,
+        "kp": 20.0,
+        "kd": 0.5,
         # termination
-        'termination_if_roll_greater_than': 0.4,
-        'termination_if_pitch_greater_than': 0.4,
-        'termination_if_height_lower_than': 0.0,
+        "termination_if_roll_greater_than": 10,  # degree
+        "termination_if_pitch_greater_than": 10,
         # base pose
-        'base_init_pos': [0.0, 0.0, 0.42],
-        'base_init_quat': [1.0, 0.0, 0.0, 0.0],
-        # random push
-        'push_interval_s': -1,
-        'max_push_vel_xy': 1.0,
-        # time (second)
-        'episode_length_s': 20.0,
-        'resampling_time_s': 4.0,
-        'command_type': 'ang_vel_yaw',  # 'ang_vel_yaw' or 'heading'
-        'action_scale': 0.25,
-        'action_latency': 0.02,
-        'clip_actions': 100.0,
-        'send_timeouts': True,
-        'control_freq': 50,
-        'decimation': 4,
-        'feet_geom_offset': 1,
-        'use_terrain': False,
-        # domain randomization
-        'randomize_friction': True,
-        'friction_range': [0.2, 1.5],
-        'randomize_base_mass': True,
-        'added_mass_range': [-1., 3.],
-        'randomize_com_displacement': True,
-        'com_displacement_range': [-0.01, 0.01],
-        'randomize_motor_strength': False,
-        'motor_strength_range': [0.9, 1.1],
-        'randomize_motor_offset': True,
-        'motor_offset_range': [-0.02, 0.02],
-        'randomize_kp_scale': True,
-        'kp_scale_range': [0.8, 1.2],
-        'randomize_kd_scale': True,
-        'kd_scale_range': [0.8, 1.2],
-        # coupling
-        'coupling': False,
+        "base_init_pos": [0.0, 0.0, 0.42],
+        "base_init_quat": [1.0, 0.0, 0.0, 0.0],
+        "episode_length_s": 20.0,
+        "resampling_time_s": 4.0,
+        "action_scale": 0.25,
+        "simulate_action_latency": True,
+        "clip_actions": 100.0,
     }
     obs_cfg = {
-        'num_obs': 9 + 3 * env_cfg['num_dofs'],
-        'num_history_obs': 1,
-        'obs_noise': {
-            'ang_vel': 0.1,
-            'gravity': 0.02,
-            'dof_pos': 0.01,
-            'dof_vel': 0.5,
+        "num_obs": 45,
+        "obs_scales": {
+            "lin_vel": 2.0,
+            "ang_vel": 0.25,
+            "dof_pos": 1.0,
+            "dof_vel": 0.05,
         },
-        'obs_scales': {
-            'lin_vel': 2.0,
-            'ang_vel': 0.25,
-            'dof_pos': 1.0,
-            'dof_vel': 0.05,
-        },
-        'num_priv_obs': 12 + 4 * env_cfg['num_dofs'],
     }
     reward_cfg = {
-        'tracking_sigma': 0.25,
-        'soft_dof_pos_limit': 0.9,
-        'base_height_target': 0.3,
-        'reward_scales': {
-            'tracking_lin_vel': 1.0,
-            'tracking_ang_vel': 0.5,
-            'lin_vel_z': -2.0,
-            'ang_vel_xy': -0.05,
-            'orientation': -10.,
-            'base_height': -50.,
-            'torques': -0.0002,
-            'collision': -1.,
-            'dof_vel': -0.,
-            'dof_acc': -2.5e-7,
-            'feet_air_time': 1.0,
-            'collision': -1.,
-            'action_rate': -0.01,
+        "tracking_sigma": 0.25,
+        "base_height_target": 0.3,
+        "feet_height_target": 0.075,
+        "reward_scales": {
+            "tracking_lin_vel": 1.0,
+            "tracking_ang_vel": 0.2,
+            "lin_vel_z": -1.0,
+            "base_height": -50.0,
+            "action_rate": -0.005,
+            "similar_to_default": -0.1,
         },
     }
-    command_cfg = {import argparse
-    import copy
-    import os
-    import pickle
-    
-    import numpy as np
-    import torch
-    from reward_wrapper import Backflip
-    from rsl_rl.runners import OnPolicyRunner
-    
-    import genesis as gs
-    
-    def export_policy_as_jit(actor_critic, path, name):
-        os.makedirs(path, exist_ok=True)
-        path = os.path.join(path, f'{name}.pt')
-        model = copy.deepcopy(actor_critic.actor).to('cpu')
-        traced_script_module = torch.jit.script(model)
-        traced_script_module.save(path)
-    
-    def main():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-e', '--exp_name', type=str, default='backflip')
-        parser.add_argument('-v', '--vis', action='store_true', default=True)
-        parser.add_argument('-c', '--cpu', action='store_true', default=False)
-        parser.add_argument('-r', '--record', action='store_true', default=False)
-        parser.add_argument('--ckpt', type=int, default=1000)
-        args = parser.parse_args()
-    
-        gs.init(backend=gs.cpu if args.cpu else gs.gpu)
-    
-        env_cfg, obs_cfg, reward_cfg, command_cfg = pickle.load(
-            open(f'logs/{args.exp_name}/cfgs.pkl', 'rb')
-        )
-        reward_cfg['reward_scales'] = {"feet_distance": 1}
-    
-        env = Backflip(
-            num_envs=1,
-            env_cfg=env_cfg,
-            obs_cfg=obs_cfg,
-            reward_cfg=reward_cfg,
-            command_cfg=command_cfg,
-            show_viewer=args.vis,
-            eval=True,
-            debug=True,
-        )
-    
-        log_dir = f'logs/{args.exp_name}'
-        jit_ckpt_path = os.path.join(log_dir, 'exported', args.exp_name + f'_ckpt{args.ckpt}.pt')
-        if os.path.exists(jit_ckpt_path):
-            policy = torch.jit.load(jit_ckpt_path)
-            policy.to(device='cuda:0')
-        else:
-            args.max_iterations = 1
-            from train_backflip import get_train_cfg
-            runner = OnPolicyRunner(env, get_train_cfg(args), log_dir, device='cuda:0')
-    
-            resume_path = os.path.join(log_dir, f'model_{args.ckpt}.pt')
-            runner.load(resume_path)
-            path = os.path.join(log_dir, 'exported')
-            export_policy_as_jit(runner.alg.actor_critic, path, args.exp_name + f'_ckpt{args.ckpt}')
-                
-            policy = runner.get_inference_policy(device='cuda:0')
-    
-        env.reset()
-        obs = env.get_observations()
-    
-        with torch.no_grad():
-            stop = False
-            n_frames = 0
-            if args.record:
-                env.start_recording(record_internal=False)
-            while not stop:
-                actions = policy(obs)
-                obs, _, rews, dones, infos = env.step(actions)
-                n_frames += 1
-                if args.record:
-                    if n_frames == 100:
-                        env.stop_recording("backflip.mp4")
-                        exit()
-    
-    if __name__ == '__main__':
-        main()
-        'num_commands': 4,
-        'lin_vel_x_range': [-1.0, 1.0],
-        'lin_vel_y_range': [-1.0, 1.0],
-        'ang_vel_range': [-1.0, 1.0],
+    command_cfg = {
+        "num_commands": 3,
+        "lin_vel_x_range": [0.5, 0.5],
+        "lin_vel_y_range": [0, 0],
+        "ang_vel_range": [0, 0],
     }
 
     return env_cfg, obs_cfg, reward_cfg, command_cfg
@@ -274,75 +137,38 @@ def get_cfgs():
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--exp_name', type=str, default='Go2')
-    parser.add_argument('-v', '--vis', action='store_true', default=False)
-    parser.add_argument('-c', '--cpu', action='store_true', default=False)
-    parser.add_argument('-B', '--num_envs', type=int, default=10000)
-    parser.add_argument('--max_iterations', type=int, default=1000)
-    parser.add_argument('--resume', type=str, default=None)
-    parser.add_argument('-o', '--offline', action='store_true', default=False)
-
-    parser.add_argument('--eval', action='store_true', default=False)
-    parser.add_argument('--debug', action='store_true', default=False)
-    parser.add_argument('--ckpt', type=int, default=1000)
+    parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
+    parser.add_argument("-B", "--num_envs", type=int, default=4096)
+    parser.add_argument("--max_iterations", type=int, default=100)
     args = parser.parse_args()
 
-    if args.debug:
-        args.vis = True
-        args.offline = True
-        args.num_envs = 1
+    gs.init(logging_level="warning")
 
-    gs.init(
-        backend=gs.cpu if args.cpu else gs.gpu,
-        logging_level='warning',
-    )
-
-    log_dir = f'logs/{args.exp_name}'
+    log_dir = f"logs/{args.exp_name}"
     env_cfg, obs_cfg, reward_cfg, command_cfg = get_cfgs()
+    train_cfg = get_train_cfg(args.exp_name, args.max_iterations)
 
     if os.path.exists(log_dir):
         shutil.rmtree(log_dir)
     os.makedirs(log_dir, exist_ok=True)
 
-    env = Go2(
-        num_envs=args.num_envs,
-        env_cfg=env_cfg,
-        obs_cfg=obs_cfg,
-        reward_cfg=reward_cfg,
-        command_cfg=command_cfg,
-        show_viewer=args.vis,
-        eval=args.eval,
-        debug=args.debug,
+    env = Go2Env(
+        num_envs=args.num_envs, env_cfg=env_cfg, obs_cfg=obs_cfg, reward_cfg=reward_cfg, command_cfg=command_cfg
     )
 
-    runner = OnPolicyRunner(env, get_train_cfg(args), log_dir, device='cuda:0')
-
-    if args.resume is not None:
-        resume_dir = f'logs/{args.resume}'
-        resume_path = os.path.join(resume_dir, f'model_{args.ckpt}.pt')
-        print('==> resume training from', resume_path)
-        runner.load(resume_path)
-
-    wandb.init(project='genesis', name=args.exp_name, dir=log_dir, mode='offline' if args.offline else 'online')
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device="cuda:0")
 
     pickle.dump(
-        [env_cfg, obs_cfg, reward_cfg, command_cfg],
-        open(f'{log_dir}/cfgs.pkl', 'wb'),
+        [env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg],
+        open(f"{log_dir}/cfgs.pkl", "wb"),
     )
 
     runner.learn(num_learning_iterations=args.max_iterations, init_at_random_ep_len=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
-
-'''
-# training
-python train_backflip.py -e EXP_NAME
-
-# evaluation
-python eval_backflip.py -e EXP_NAME --ckpt NUM_CKPT
-'''
-
-# auf ebenen terrain so schnell wie möglich laufen
+"""
+python train_walk.py -e go2-walking -B 4096 --max_iterations 100
+"""
